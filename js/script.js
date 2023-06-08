6440fb1709bc931c10466399c98b4b6b20568102
Abuzar Tamboli <abuzart1999@gmail.com>
2023-06-08 10:49:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ class Symbol {
     this.text = this.characters.charAt(
       Math.floor(Math.random() * this.characters.length)
     );
-    ctx.fillStyle = "#ffffff";
+    ctx.fillStyle = "#008F11";
     ctx.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
     if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
